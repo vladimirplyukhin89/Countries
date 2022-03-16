dc2e0583a833245b5cf410246f1bd4d7bfc5ed8b
Vladimir Plyukhin <vladimirplyukhin89@gmail.com>
2022-03-16 10:11:49 -1100

    Fix a bug with deploy project

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -87,89 +87,89 @@ const Tag = styled.span`
 `;
 
 export const Info = (props) => {
-    const {
-        name,
-        nativeName,
-        flag,
-        region,
-        subRegion,
-        capital,
-        population,
-        topLevelDomain,
-        currencies = [],
-        languages = [],
-        borders = [],
-    } = props;
+  const {
+    name,
+    nativeName,
+    flag,
+    region,
+    subRegion,
+    capital,
+    population,
+    topLevelDomain,
+    currencies = [],
+    languages = [],
+    borders = [],
+  } = props;
 
-    const [neighbors, setNeighbors] = useState([]);
+  const [neighbors, setNeighbors] = useState([]);
 
-    useEffect(() => {
-        axios.get(filterByCode(borders)).then(
-            ({ data }) => setNeighbors(data.map(c => c.name))
-        );
-    }, [borders]);
-
-    return (
-        <Wrapper>
-            <InfoImage src={flag} alt={name} />
-
-            <div>
-                <InfoTitle>{name}</InfoTitle>
-                <ListGroup>
-                    <List>
-                        <ListItem>
-                            <b>Native Name:</b> {nativeName}
-                        </ListItem>
-                        <ListItem>
-                            <b>Population:</b> {population}
-                        </ListItem>
-                        <ListItem>
-                            <b>Region:</b> {region}
-                        </ListItem>
-                        <ListItem>
-                            <b>Sub Region:</b> {subRegion}
-                        </ListItem>
-                        <ListItem>
-                            <b>Capital:</b> {capital}
-                        </ListItem>
-                    </List>
-                    <List>
-                        <ListItem>
-                            <b>Top Level Domain: </b>{` `}
-                            {topLevelDomain.map(d =>
-                            (
-                                <span key={d}>{d}</span>
-                            ))}
-                        </ListItem>
-                        <ListItem>
-                            <b>Currency: </b>{` `}
-                            {currencies.map(c =>
-                            (
-                                <span key={c.code}>{c.name} </span>
-                            ))}
-                        </ListItem>
-                        <ListItem>
-                            <b>Language: </b>{` `}
-                            {languages.map(l =>
-                            (
-                                <span key={l.name}>{l.name}</span>
-                            ))}
-                        </ListItem>
-                    </List>
-                </ListGroup>
-                <Meta>
-                    <b>Border Countries:</b>
-                    {!borders.length ? (
-                        <span> There are no border countries</span>
-                    ) : (
-                        <TagGroup>
-                            {neighbors.map(b => (
-                                <Tag key={b}>{b}</Tag>
-                            ))}
-                        </TagGroup>
-                    )}
-                </Meta>
-            </div>
-        </Wrapper>
+  useEffect(() => {
+    axios.get(filterByCode(borders)).then(
+      ({ data }) => setNeighbors(data.map(c => c.name))
     );
+  }, [borders]);
+
+  return (
+    <Wrapper>
+      <InfoImage src={flag} alt={name} />
+
+      <div>
+        <InfoTitle>{name}</InfoTitle>
+        <ListGroup>
+          <List>
+            <ListItem>
+              <b>Native Name:</b> {nativeName}
+            </ListItem>
+            <ListItem>
+              <b>Population:</b> {population}
+            </ListItem>
+            <ListItem>
+              <b>Region:</b> {region}
+            </ListItem>
+            <ListItem>
+              <b>Sub Region:</b> {subRegion}
+            </ListItem>
+            <ListItem>
+              <b>Capital:</b> {capital}
+            </ListItem>
+          </List>
+          <List>
+            <ListItem>
+              <b>Top Level Domain: </b>{` `}
+              {topLevelDomain.map(d =>
+              (
+                <span key={d}>{d}</span>
+              ))}
+            </ListItem>
+            <ListItem>
+              <b>Currency: </b>{` `}
+              {currencies.map(c =>
+              (
+                <span key={c.code}>{c.name} </span>
+              ))}
+            </ListItem>
+            <ListItem>
+              <b>Language: </b>{` `}
+              {languages.map(l =>
+              (
+                <span key={l.name}>{l.name} </span>
+              ))}
+            </ListItem>
+          </List>
+        </ListGroup>
+        <Meta>
+          <b>Border Countries:</b>
+          {!borders.length ? (
+            <span> There are no border countries</span>
+          ) : (
+            <TagGroup>
+              {neighbors.map(b => (
+                <Tag key={b}>{b}</Tag>
+              ))}
+            </TagGroup>
+          )}
+        </Meta>
+      </div>
+    </Wrapper>
+  );
 };
