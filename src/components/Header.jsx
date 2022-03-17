a990bb1bb9799f5c00e8d0913a9d328f617de804
Vladimir Plyukhin <vladimirplyukhin89@gmail.com>
2022-03-16 15:38:46 -1100

    Chose your favorite country

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -46,7 +46,7 @@ export const Header = () => {
         <HeaderEl>
             <Container>
                 <Wrapper>
-                    <Title>Where is the world?</Title>
+                    <Title>Chose your favorite country</Title>
                     <ModeSwitcher onClick={toggleTheme}>
                         {theme === 'light' ? (
                             <IoMoonOutline size='14px' />
